761d941461f9aab1b4fb9c23aa165e0206e80e67
Andy Willis <github@awillis.fastmail.fm>
2017-09-25 09:49:28 +0100

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# uws-metafilter-favouriteers
+# metafilter-favouriteers
+
+Unofficial Metafilter web extension.
 
 Update of Michael Devore's [Howls Of Outrage](https://greasyfork.org/en/scripts/5439-howlsofoutrage) Greasemonkey script to add a mouseover popup of favouriteers to each post comment on Metafilter.
+
+The limit of favouriteers to see is capped at 150 due to display constraints.
--- content/index.js
+++ content/index.js
@@ -14,19 +14,41 @@
     console.log(error);
   }
 
-  function showPopup(names, e) {
+  function getSite() {
+    return document.location.host.split('.')[0];
+  }
+
+  function hasSite(site) {
+    return [
+      'www', 'ask', 'metatalk', 'fanfare',
+      'projects', 'music', 'podcast'
+    ].includes(site);
+  }
+
+  function maxLengthName(names) {
+    return Math.max.apply(null, names.map(name => name.length));
+  }
+
+  function showPopup(names, site, e) {
     const popup = qs('#popup');
-    popup.innerHTML = names.join(', ');
-    popup.style.left = `${e.pageX}px`;
-    popup.style.top = `${e.pageY}px`;
+    const joinedNames = names.join(', ');
+    const len = joinedNames.length;
+    const min = maxLengthName(names);
+    let width = (min * 8) + (((len * 6) / 450) * 60);
+    if (width > 600) width = 600;
+    popup.innerHTML = joinedNames;
+    popup.classList.add(`${site}color`);
+    popup.style.width = `${width}px`;
+    popup.style.left = `${e.pageX - (width / 2)}px`;
+    popup.style.top = `${e.pageY + 10}px`;
     popup.style.display = 'inline';
     popup.style.position = 'absolute';
   }
 
-  function handleData(data, e) {
+  function handleData(data, site, e) {
     const favourites = data.favorites;
-    const names = favourites.map(favourite => favourite.user_name);
-    showPopup(names, e);
+    let names = favourites.map(favourite => favourite.user_name);
+    showPopup(names, site, e);
   }
 
   function addPopup() {
@@ -36,7 +58,13 @@
 
   function removeTitles(favourites) {
     favourites.forEach(function (favourite) {
-      favourite.removeAttribute('title');
+      const fav = favourite.getAttribute('title');
+      const regex = /^\d+/;
+      if (regex.test(fav)) {
+        const count = fav.match(regex)[0];
+        favourite.setAttribute('data-favcount', count);
+        favourite.removeAttribute('title');
+      }
     });
   }
 
@@ -47,11 +75,16 @@
     return `https://www.metafilter.com/favorited/${sid}/${lid}/json`;
   }
 
-  function showFavourites(e) {
-    fetch(getEndpoint(e.target.href))
-      .then(response => response.json())
-      .then(data => handleData(data, e))
-      .catch(handleError);
+  function showFavourites(site, e) {
+    if (e.target.getAttribute('data-favcount') < 150) {
+      fetch(getEndpoint(e.target.href))
+        .then(response => response.text())
+        .then(json => JSON.parse(json.replace(/\\'/g, '\'')))
+        .then(data => handleData(data, site, e))
+        .catch(handleError);
+    } else {
+      showPopup(['Too many names to show.'], site, e);
+    }
   }
 
   function hideFavourites() {
@@ -59,9 +92,9 @@
     clearTimeout(timer);
   }
 
-  function pauseHover(e) {
+  function pauseHover(site, e) {
     timer = setTimeout(function () {
-      showFavourites(e);
+      showFavourites(site, e);
     }, 300);
   }
 
@@ -69,18 +102,21 @@
     return qsa('span[id^="favcnt"] a');
   }
 
-  function addListeners(favourites) {
+  function addListeners(favourites, site) {
     favourites.forEach(function (favourite) {
-      favourite.addEventListener('mouseover', pauseHover, false);
+      favourite.addEventListener('mouseover', pauseHover.bind(this, site), false);
       favourite.addEventListener('mouseout', hideFavourites, false);
     });
   }
 
   function init() {
-    addPopup();
-    const favourites = getLinks();
-    removeTitles(favourites);
-    addListeners(favourites);
+    const site = getSite();
+    if (hasSite(site)) {
+      addPopup();
+      const favourites = getLinks();
+      removeTitles(favourites);
+      addListeners(favourites, site);
+    }
   }
 
   init();
